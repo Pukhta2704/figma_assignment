b352bd142fcbbfca97c1505bc1f354d7909824cd
Imtiyaz Pukhta <pukhtaib@gmail.com>
2022-05-10 11:11:36 +0530

    api integration

---
+++ index.js
@@ -0,0 +1,50 @@
+let loading = true
+document.querySelector(".lds-roller").style = "display:block;"
+let j = Math.floor(Math.random() * 500)
+// let j = 3
+let result = []
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Host":
+      "list-of-freshwater-aquarium-fish-species.p.rapidapi.com",
+    "X-RapidAPI-Key": "27c0185529msh8e889740729f035p1dd26djsn3db436618e85"
+  }
+}
+fetch(
+  "https://list-of-freshwater-aquarium-fish-species.p.rapidapi.com/species",
+  options
+)
+  .then((response) => response.json())
+  .then((response) => {
+    result = response
+    setData()
+  })
+  .catch((err) => console.error(err))
+document.querySelector(".categories button").addEventListener("click", () => {
+  setData()
+})
+const setData = async () => {
+document.querySelector(".lds-roller").style = "display:block;"
+  for (let i = j; i < j + 3; i++) {
+    if (await checkImage(result[i].imageURL)) {
+      const newElement = `
+        <div>
+          <img src="${result[i].imageURL}" alt="" />
+          <p>${result[i].name}</p>
+        </div>
+      `
+      document.getElementById("category-card").innerHTML += newElement
+    } else {
+      j++
+    }
+  }
+  j += 3
+  document.querySelector(".lds-roller").style = "display:none;"
+}
+async function checkImage(url) {
+  const res = await fetch(url)
+  const buff = await res.blob()
+
+  return buff.type.startsWith("image/")
+}
